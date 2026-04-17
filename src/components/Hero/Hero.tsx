75dbdc3c8e2987e7e7a4a1de70eebd2efaeec7d5
tekendra025 <eagleyt023@gmail.com>
2026-04-17 11:29:36 +0530

    Hero Section Updated

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <img
         src={HeroImage}
         alt="Hero"
-        className="absolute w-full h-full object-cover"
+        className="absolute w-full h-full object-cover object-[80%_center] "
       />
 
        {/* Dark Overlay */}
@@ -17,13 +17,13 @@ const Hero = () => {
         
         {/* left side */}
         <div className='absolute top-1/2 left-5 text-white'>
-          <button className="bg-primary px-4 py-2 rounded-4xl font-semibold cursor-pointer">New Arrivals 2026</button>
+          <button className="bg-primary px-4 py-2 rounded-4xl font-semibold cursor-pointer hover:bg-yellow-600 duration-500">New Arrivals 2026</button>
           <h2 className='text-3xl font-bold my-3'>Discover Your Perfect Style</h2>
           <p className=' text-gray-300 mb-5'>explore our latest collection of trendy fashion. Quality meets affordability</p>
          
           <div className='flex gap-3'>
-             <button className="bg-primary px-4 py-2 rounded-md font-semibold cursor-pointer">Shop Now</button>
-             <button className=" border-2 rounded-md px-4 py-2 font-semibold cursor-pointer">View collection</button>
+             <button className="bg-primary px-4 py-2 rounded-md font-semibold cursor-pointer hover:bg-yellow-600 duration-500">Shop Now</button>
+             <button className=" border-2 rounded-md px-4 py-2 font-semibold cursor-pointer hover:border-white/60 duration-500 capitalize">View collection</button>
           </div>
         </div>
       </div>
